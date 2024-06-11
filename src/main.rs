e2786a7198c04837f7fc49588d545da5eac5afa4
VisheshRawal <vp2005rawal@gmail.com>
2024-06-11 22:54:46 +0530

    Created Unit Tests

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,16 @@
-use std::{env, fs};
+use std::{env, process};
+use minigrep::Config;
 
 fn main(){
     let args: Vec<String> = env::args().collect();
 
-    if args.len() == 1 {
-        println!("Error in {}, as the function is out of bound", args[0]);
-    } else {
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem Parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    let config = parse_config(&args);
-
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
-
-    let contents = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
-
-    println!("With text:\n{contents}");
-}
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-fn parse_config(args: &[String]) -> Config{
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-
-    return Config{ query, file_path }
+    if let Err(e) = minigrep::run(config){
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
 }
